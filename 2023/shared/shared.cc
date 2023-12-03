f1ccd0ce10b6d108adc7c79f193115835234a856
Bryan Oltman <bryanoltman@gmail.com>
2023-12-03 00:54:55 -0500

    Add shared.cc

---
+++ 2023/shared/shared.cc
@@ -0,0 +1,20 @@
+#include "shared.h"
+
+vector<string> ReadLines(std::string file_path)
+{
+    vector<string> lines;
+    ifstream file(file_path);
+
+    if (!file.is_open()) {
+        cerr << "Unable to open file: " << file_path << endl;
+        exit(-1);
+    }
+
+    string line;
+    while (getline(file, line)) {
+        lines.push_back(line);
+    }
+    file.close();
+
+    return lines;
+}
--- 2023/shared/shared.h
+++ 2023/shared/shared.h
@@ -5,21 +5,4 @@
 
 using namespace std;
 
-vector<string> ReadLines(std::string file_path)
-{
-    vector<string> lines;
-    ifstream file(file_path);
-
-    if (!file.is_open()) {
-        cerr << "Unable to open file: " << file_path << endl;
-        exit(-1);
-    }
-
-    string line;
-    while (getline(file, line)) {
-        lines.push_back(line);
-    }
-    file.close();
-
-    return lines;
-}
+vector<string> ReadLines(std::string file_path);
