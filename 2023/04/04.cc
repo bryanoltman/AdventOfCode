363e18815486292193b7425062c71c8a2afd5cd2
Bryan Oltman <bryanoltman@gmail.com>
2023-12-04 21:52:38 -0500

    solve day 4 part 2

--- 2023/04/04.cc
+++ 2023/04/04.cc
@@ -37,6 +37,17 @@ Card::Card(string &str)
     drawn_numbers = set<int>(drawn_numbers_str.begin(), drawn_numbers_str.end());
 }
 
+size_t Card::WinningNumberCount()
+{
+    size_t count = 0;
+    for (int number : drawn_numbers) {
+        if (winning_numbers.find(number) != winning_numbers.end()) {
+            count++;
+        }
+    }
+    return count;
+}
+
 vector<Card> ParseInput(string file_path)
 {
     auto lines = ReadLines(file_path);
@@ -52,30 +63,42 @@ int PartOne(vector<Card> cards)
 {
     int score = 0;
     for (auto &card : cards) {
-        int num_winning_numbers = 0;
-        for (int number : card.drawn_numbers) {
-            if (card.winning_numbers.find(number) != card.winning_numbers.end()) {
-                num_winning_numbers++;
-            }
-        }
-
-        // cout << "Card " << card.number << " has " << num_winning_numbers << " winning numbers"
-        //      << endl;
+        int num_winning_numbers = card.WinningNumberCount();
         if (num_winning_numbers > 0) {
-            // cout << "score is " << (1 << (num_winning_numbers - 1)) << endl;
             score += 1 << (num_winning_numbers - 1);
         }
     }
     return score;
 }
 
+int PartTwo(vector<Card> cards)
+{
+    vector<int> card_counts(cards.size(), 1);
+
+    for (size_t i = 0; i < cards.size(); i++) {
+        auto card = cards[i];
+        int num_winning_numbers = card.WinningNumberCount();
+        for (int num_cards = 0; num_cards < card_counts[i]; num_cards++) {
+            for (auto j = i + 1; j <= num_winning_numbers + i && j < cards.size(); j++) {
+                card_counts[j] += 1;
+            }
+        }
+    }
+
+    int num_cards = 0;
+    for (auto count : card_counts) {
+        num_cards += count;
+    }
+    return num_cards;
+}
+
 int main(int argc, char const *argv[])
 {
-    // auto input = ParseInput("inputs/04.txt");
-    auto input = ParseInput("inputs/04.test.txt");
+    auto input = ParseInput("inputs/04.txt");
+    // auto input = ParseInput("inputs/04.test.txt");
 
     cout << "Part One: " << PartOne(input) << endl;
-    // cout << "Part Two: " << PartTwo(input) << endl;
+    cout << "Part Two: " << PartTwo(input) << endl;
 
     return 0;
 }
--- 2023/04/04.h
+++ 2023/04/04.h
@@ -6,6 +6,8 @@ using namespace std;
 class Card {
 public:
     Card(string &str);
+    size_t WinningNumberCount();
+
     int number;
     set<int> winning_numbers;
     set<int> drawn_numbers;
