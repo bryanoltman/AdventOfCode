ec2bf8ee4fb30f3eb7fd365bb9da2b6a0d07bbbe
Bryan Oltman <bryanoltman@gmail.com>
2023-12-04 21:54:45 -0500

    prep for day 5

---
+++ 2023/05/05.cc
@@ -0,0 +1,12 @@
+
+
+int main(int argc, char const *argv[])
+{
+    // auto input = ParseInput("inputs/05.txt");
+    // auto input = ParseInput("inputs/05.test.txt");
+
+    // cout << "Part One: " << PartOne(input) << endl;
+    // cout << "Part Two: " << PartTwo(input) << endl;
+
+    return 0;
+}
