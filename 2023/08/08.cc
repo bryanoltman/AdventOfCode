c8a877845b3921e3651534f81d0e7fcdf295eec5
Bryan Oltman <bryanoltman@gmail.com>
2023-12-08 19:51:29 -0500

    implement non-brute-force solution for day 8 part 2

--- 2023/08/08.cc
+++ 2023/08/08.cc
@@ -1,7 +1,7 @@
 #include <cstddef>
 #include <iostream>
+#include <numeric>
 #include <regex>
-#include <set>
 
 #include "08/08.h"
 #include "shared/shared.h"
@@ -10,95 +10,128 @@ using namespace std;
 
 static auto node_line_regex = regex(R"R((\w\w\w) = \((\w\w\w), (\w\w\w)\))R");
 
-Input ParseInput(const string &filename) {
-  auto lines = ReadLines(filename);
-  Input ret;
+Input ParseInput(const string &filename)
+{
+    auto lines = ReadLines(filename);
+    Input ret;
 
-  auto lines_itr = lines.begin();
-  ret.directions = *lines_itr++;
-  lines_itr++; // Skip the blank line
-  for (; lines_itr != lines.end(); lines_itr++) {
-    smatch matches;
-    regex_match(*lines_itr, matches, node_line_regex);
-    string node = matches[1].str();
-    string left = matches[2].str();
-    string right = matches[3].str();
-    ret.nodes[node] = make_pair(left, right);
-  }
+    auto lines_itr = lines.begin();
+    ret.directions = *lines_itr++;
+    lines_itr++; // Skip the blank line
+    for (; lines_itr != lines.end(); lines_itr++) {
+        smatch matches;
+        regex_match(*lines_itr, matches, node_line_regex);
+        string node = matches[1].str();
+        string left = matches[2].str();
+        string right = matches[3].str();
+        ret.nodes[node] = make_pair(left, right);
+    }
 
-  return ret;
+    return ret;
 }
 
-size_t PartOne(Input input) {
-  size_t num_steps = 0;
-  size_t directions_index = 0;
-  string current_node = "AAA";
-  while (current_node != "ZZZ") {
-    char current_direction = input.directions[directions_index];
-    directions_index++;
-    if (directions_index == input.directions.length()) {
-      directions_index = 0;
+size_t PartOne(Input input)
+{
+    size_t num_steps = 0;
+    size_t directions_index = 0;
+    string current_node = "AAA";
+    while (current_node != "ZZZ") {
+        char current_direction = input.directions[directions_index];
+        directions_index++;
+        if (directions_index == input.directions.length()) {
+            directions_index = 0;
+        }
+
+        auto node_pair = input.nodes[current_node];
+        if (current_direction == 'L') {
+            current_node = node_pair.first;
+        } else if (current_direction == 'R') {
+            current_node = node_pair.second;
+        } else {
+            cout << "Unknown direction found " << current_direction << endl;
+            exit(-1);
+        }
+
+        num_steps++;
     }
 
-    auto node_pair = input.nodes[current_node];
-    if (current_direction == 'L') {
-      current_node = node_pair.first;
-    } else if (current_direction == 'R') {
-      current_node = node_pair.second;
-    } else {
-      cout << "Unknown direction found " << current_direction << endl;
-      exit(-1);
-    }
-
-    num_steps++;
-  }
-
-  return num_steps;
+    return num_steps;
 }
 
-bool PartTwoIsDone(vector<string> nodes) {
-  for (auto node : nodes) {
-    if (node.back() != 'Z') {
-      return false;
+bool PartTwoIsEndNode(string node) { return node.back() == 'Z'; }
+
+pair<size_t, size_t> FindPartTwoFirstMatchAndCycleSize(Input input, string start_node)
+{
+    size_t directions_index = 0;
+    size_t node_matches = 0;
+    string current_node = start_node;
+    size_t steps = 0;
+    size_t first_match = SIZE_T_MAX;
+    vector<size_t> match_steps;
+    // 5 is arbitrarily chosen to find a cycle
+    while (node_matches < 5) {
+        char current_direction = input.directions[directions_index];
+        directions_index++;
+        if (directions_index == input.directions.length()) {
+            directions_index = 0;
+        }
+
+        auto node_pair = input.nodes[current_node];
+        if (current_direction == 'L') {
+            current_node = node_pair.first;
+        } else if (current_direction == 'R') {
+            current_node = node_pair.second;
+        } else {
+            cout << "Unknown direction found " << current_direction << endl;
+            exit(-1);
+        }
+
+        if (PartTwoIsEndNode(current_node)) {
+            if (first_match == SIZE_T_MAX) {
+                first_match = steps;
+            }
+            node_matches++;
+            match_steps.push_back(steps);
+        }
+
+        steps++;
     }
-  }
-  return true;
+
+    return make_pair(first_match, match_steps[4] - match_steps[3]);
 }
 
-size_t PartTwo(Input input) {
-  size_t num_steps = 0;
-  size_t directions_index = 0;
-  vector<string> current_nodes;
-  for (auto node_entry : input.nodes) {
-    auto node = node_entry.first;
-    if (node.back() == 'A') {
-      current_nodes.push_back(node);
-    }
-  }
+// Returns LCM of array elements
+// Adapted from https://www.geeksforgeeks.org/lcm-of-given-array-elements/
+size_t findlcm(vector<size_t> v)
+{
+    // Initialize result
+    size_t ret = v[0];
 
-  while (!PartTwoIsDone(current_nodes)) {
-    char current_direction = input.directions[directions_index];
-    directions_index++;
-    if (directions_index == input.directions.length()) {
-      directions_index = 0;
+    for (int i = 1; i < v.size(); i++) {
+        ret = (v[i] * ret) / gcd(v[i], ret);
     }
 
-    vector<string> new_nodes;
+    return ret;
+}
+
+size_t PartTwo(Input input)
+{
+    size_t num_steps = 0;
+    vector<string> current_nodes;
+    for (auto node_entry : input.nodes) {
+        auto node = node_entry.first;
+        if (node.back() == 'A') {
+            current_nodes.push_back(node);
+        }
+    }
+
+    vector<size_t> cycle_sizes;
     for (auto node : current_nodes) {
-      auto node_pair = input.nodes[node];
-      if (current_direction == 'L') {
-        new_nodes.push_back(node_pair.first);
-      } else if (current_direction == 'R') {
-        new_nodes.push_back(node_pair.second);
-      } else {
-        cout << "Unknown direction found " << current_direction << endl;
-        exit(-1);
-      }
+        auto [first_match, cycle_size] = FindPartTwoFirstMatchAndCycleSize(input, node);
+        cycle_sizes.push_back(cycle_size);
     }
 
-    current_nodes = new_nodes;
-    num_steps++;
-  }
+    return findlcm(cycle_sizes);
 
-  return num_steps;
+    return num_steps;
 }
