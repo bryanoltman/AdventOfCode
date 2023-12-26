356fdf30b3e5d6d39b16a9935d242418e9aa9b60
Bryan Oltman <bryan@shorebird.dev>
2023-12-25 21:53:14 -0500

    solve day 20 part 1

--- 2023/20/20.cc
+++ 2023/20/20.cc
@@ -1,6 +1,7 @@
 #include <cstddef>
 #include <iostream>
 #include <map>
+#include <queue>
 #include <regex>
 
 #include "20/20.h"
@@ -18,6 +19,7 @@ Input ParseInput(const string &filename)
   for (auto &line : lines) {
     vector<string> destinations;
     Module mod;
+    mod.is_on = false;
     if (regex_match(line, match, broadcaster_regex)) {
       mod.name = "broadcaster";
       destinations = SplitString(match[1].str(), ',');
@@ -25,9 +27,11 @@ Input ParseInput(const string &filename)
     else if (regex_match(line, match, module_regex)) {
       auto type = match[1].str();
       if (type == "%") {
+        mod.is_conjunction = false;
         mod.is_flip_flop = true;
       }
       else if (type == "&") {
+        mod.is_flip_flop = false;
         mod.is_conjunction = true;
       }
       else {
@@ -36,10 +40,11 @@ Input ParseInput(const string &filename)
       }
 
       mod.name = match[2].str();
-      destinations = SplitString(match[1].str(), ',');
+      destinations = SplitString(match[3].str(), ',');
     }
     else {
-      cout << "Unknown line: " << line << endl;
+      cerr << "Unknown line: " << line << endl;
+      exit(-1);
     }
 
     for (auto &dest : destinations) {
@@ -49,13 +54,87 @@ Input ParseInput(const string &filename)
     modules[mod.name] = mod;
   }
 
+  for (auto &mod : modules) {
+    for (auto &dest : mod.second.destinations) {
+      modules[dest].inputs[mod.first] = false;
+    }
+  }
+
   return modules;
+}
+
+// Returns the number of high and low pulses sent.
+pair<size_t, size_t> PushButton(Input &input)
+{
+  pair<size_t, size_t> ret = {0, 0};
+  // source, dest, is_high_pulse
+  queue<tuple<string, string, bool>> pulses;
+  Module &broadcaster = input["broadcaster"];
+
+  ret.second++;
+
+  for (auto &dest : broadcaster.destinations) {
+    pulses.push({broadcaster.name, dest, false});
+  }
+
+  while (!pulses.empty()) {
+    auto pulse = pulses.front();
+    pulses.pop();
+
+    auto source_name = get<0>(pulse);
+    auto dest_name = get<1>(pulse);
+    auto is_high_pulse = get<2>(pulse);
+
+    if (is_high_pulse) {
+      ret.first++;
+    }
+    else {
+      ret.second++;
+    }
+
+    auto &dest = input[dest_name];
+    if (dest.is_flip_flop) {
+      if (is_high_pulse) {
+        continue;
+      }
+
+      bool is_sending_high_pulse = !dest.is_on;
+      for (auto &new_dest : dest.destinations) {
+        pulses.push({dest_name, new_dest, is_sending_high_pulse});
+      }
+      dest.is_on = !dest.is_on;
+    }
+    else {
+      // mod is conjunction
+      dest.inputs[source_name] = is_high_pulse;
+      bool are_all_inputs_high = true;
+      for (auto &input : dest.inputs) {
+        if (!input.second) {
+          are_all_inputs_high = false;
+          break;
+        }
+      }
+
+      bool is_sending_high_pulse = !are_all_inputs_high;
+      for (auto &new_dest : dest.destinations) {
+        pulses.push({dest_name, new_dest, is_sending_high_pulse});
+      }
+    }
+  }
+
+  return ret;
 }
 
 size_t PartOne(Input input)
 {
-  size_t ret = 0;
-  return ret;
+  pair<size_t, size_t> ret = {0, 0};
+  for (int i = 0; i < 1000; i++) {
+    auto pulses = PushButton(input);
+    ret.first += pulses.first;
+    ret.second += pulses.second;
+  }
+
+  return ret.first * ret.second;
 }
 
 size_t PartTwo(Input input)
--- 2023/20/20_test.cc
+++ 2023/20/20_test.cc
@@ -8,19 +8,21 @@ static auto TAGS = "Day 20";
 
 TEST_CASE("ParseInput", TAGS)
 {
-  auto input = ParseInput("inputs/20.test.txt");
+  auto input = ParseInput("inputs/20.test2.txt");
   Module broadcast_module = input["broadcaster"];
-  REQUIRE(broadcast_module.destinations == (vector<string>){"a", "b", "c"});
+  REQUIRE(broadcast_module.destinations == (vector<string>){"a"});
+  REQUIRE(input["a"].destinations == (vector<string>){"inv", "con"});
+  REQUIRE(input["inv"].is_conjunction);
 }
 
 TEST_CASE("Part 1", TAGS)
 {
   auto input = ParseInput("inputs/20.test.txt");
-  REQUIRE(PartOne(input) == 0);
+  REQUIRE(PartOne(input) == 32000000);
 }
 
-TEST_CASE("Part 2", TAGS)
+TEST_CASE("Part 1 (second input)", TAGS)
 {
-  auto input = ParseInput("inputs/20.test.txt");
-  REQUIRE(PartTwo(input) == 0);
+  auto input = ParseInput("inputs/20.test2.txt");
+  REQUIRE(PartOne(input) == 11687500);
 }
