e384d24403f0f726be6228f4aa580e5a6e18c940
Bryan Oltman <bryan@shorebird.dev>
2024-12-03 11:35:10 -0500

    day 3 part 2

--- 2024/Sources/Day03.swift
+++ 2024/Sources/Day03.swift
@@ -4,6 +4,8 @@ struct Day03: AdventDay {
 
   var data: String
 
+  let doRegex = try! NSRegularExpression(pattern: #"do\(\)"#)
+  let dontRegex = try! NSRegularExpression(pattern: #"don't\(\)"#)
   let mulRegex = try! NSRegularExpression(pattern: #"mul\((\d+),(\d+)\)"#)
 
   func part1() -> Int {
@@ -18,7 +20,58 @@ struct Day03: AdventDay {
       .reduce(0, +)
   }
 
+  enum ControlFlowMatch {
+    case start
+    case stop
+    case mult(Int, Int)
+  }
+
   func part2() -> Int {
-    return 0
+    var multiplicationEnabled = true
+    let dataRange = NSRange(data.startIndex..., in: data)
+    let multMatches = mulRegex.matches(in: data, range: dataRange)
+    let doMatches = doRegex.matches(in: data, range: dataRange)
+    let dontMatches = dontRegex.matches(in: data, range: dataRange)
+
+    var allMatches = [(Int, ControlFlowMatch)]()
+    for match in multMatches {
+      let range1 = Range(match.range(at: 1), in: data)!
+      let range2 = Range(match.range(at: 2), in: data)!
+      let num1 = Int(String(data[range1]))!
+      let num2 = Int(String(data[range2]))!
+      allMatches.append((match.range.location, .mult(num1, num2)))
+    }
+
+    for match in doMatches {
+      allMatches.append((match.range.location, .start))
+    }
+
+    for match in dontMatches {
+      allMatches.append((match.range.location, .stop))
+    }
+
+    allMatches.sort { (lhs, rhs) in
+      lhs.0 < rhs.0
+    }
+
+    var sum = 0
+    for match in allMatches.map({ $0.1 })  {
+      if case .start = match {
+        multiplicationEnabled = true
+        continue
+      }
+
+      if case .stop = match {
+        multiplicationEnabled = false
+        continue
+      }
+
+      if case .mult(let x, let y) = match {
+        guard multiplicationEnabled else { continue }
+        sum += x * y
+      }
+    }
+
+    return sum
   }
 }
--- 2024/Tests/Day03.swift
+++ 2024/Tests/Day03.swift
@@ -3,13 +3,14 @@ import Testing
 @testable import AdventOfCode
 
 @Suite("Day 3") struct Day03Tests {
-  let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+  let part1Input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+  let part2Input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
   @Test func Part1() {
-    #expect(Day03(data: input).part1() == 161)
+    #expect(Day03(data: part1Input).part1() == 161)
   }
 
   @Test func Part2() {
-    //    #expect(Day03(data: input).part2() == 0)
+    #expect(Day03(data: part2Input).part2() == 48)
   }
 }
