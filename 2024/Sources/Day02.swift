605fc9ca23ccd60141490c7fada697770b899014
Bryan Oltman <bryan@shorebird.dev>
2024-12-02 23:15:54 -0500

    complete day 2

--- 2024/Sources/Day02.swift
+++ 2024/Sources/Day02.swift
@@ -9,33 +9,67 @@ struct Day02: AdventDay {
       }
   }
 
-  func checkLevels(levels: [Int], increasing: Bool) -> Bool {
+  func checkLevels(levels: [Int], increasing: Bool, canSplit: Bool = false) -> Bool {
     for i in 0..<levels.count - 1 {
       let current = levels[i]
       let next = levels[i + 1]
       let diff = next - current
       if increasing && !(1..<4).contains(diff) {
-        return false
+        if canSplit {
+          var copy1 = levels
+          copy1.remove(at: i)
+          var copy2 = levels
+          copy2.remove(at: i + 1)
+          var copy3 = levels
+          if (i > 0){
+            copy3.remove(at: i - 1)
+          }
+          return checkLevels(levels: copy1, increasing: true)
+            || checkLevels(levels: copy2, increasing: true)
+            || checkLevels(levels: copy3, increasing: true)
+        }
+        else {
+          return false
+        }
       }
 
       if !increasing && !(-3..<0).contains(diff) {
-        return false
+        if canSplit {
+          var copy1 = levels
+          copy1.remove(at: i)
+          var copy2 = levels
+          copy2.remove(at: i + 1)
+          var copy3 = levels
+          if (i > 0){
+            copy3.remove(at: i - 1)
+          }
+          return checkLevels(levels: copy1, increasing: false)
+            || checkLevels(levels: copy2, increasing: false)
+            || checkLevels(levels: copy3, increasing: false)
+        }
+        else {
+          return false
+        }
       }
     }
 
     return true
   }
 
-  func isSafe(levels: [Int]) -> Bool {
-    return checkLevels(levels: levels, increasing: true)
-      || checkLevels(levels: levels, increasing: false)
+  func isSafe(levels: [Int], canSplit: Bool = false) -> Bool {
+    return checkLevels(levels: levels, increasing: true, canSplit: canSplit)
+    || checkLevels(levels: levels, increasing: false, canSplit: canSplit)
   }
 
   func part1() -> Int {
-    return entities.count(where: isSafe)
+    return entities.count { row in
+      isSafe(levels: row)
+    }
   }
 
   func part2() -> Int {
-    return 0
+    return entities.count { row in
+      isSafe(levels: row, canSplit: true)
+    }
   }
 }
