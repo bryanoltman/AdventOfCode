832065a0506e041079ef7aed85caeaf201136965
Bryan Oltman <bryan@shorebird.dev>
2024-12-04 10:41:18 -0500

    2024 day 4 setup

---
+++ 2024/Sources/Day04.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct Day04: AdventDay {
+
+  var data: String
+
+  func part1() -> Int {
+    return 0
+  }
+
+  func part2() -> Int {
+    return 0
+  }
+}
---
+++ 2024/Tests/Day04.swift
@@ -0,0 +1,26 @@
+import Testing
+
+@testable import AdventOfCode
+
+@Suite("Day 4") struct Day04Tests {
+  let input = """
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+"""
+
+  @Test func Part1() {
+//    #expect(Day03(data: part1Input).part1() == 161)
+  }
+
+  @Test func Part2() {
+//    #expect(Day03(data: part2Input).part2() == 48)
+  }
+}
