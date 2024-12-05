dd1980526c7d63b0a4673337a9c393226a621ae4
Bryan Oltman <bryan@shorebird.dev>
2024-12-05 11:20:37 -0500

    2024 day 5 part 1

---
+++ 2024/Sources/Day05.swift
@@ -0,0 +1,71 @@
+struct SortOrder {
+  let first: Int
+  let last: Int
+}
+
+struct OrderRule {
+  var before: Set<Int>
+  var after: Set<Int>
+}
+
+struct Day05: AdventDay {
+  init(data: String) {
+    self.data = data
+    let parts = data.split(separator: "\n\n")
+
+    sortOrders = parts[0].split(separator: "\n")
+      .map({ line in
+        let nums = line.split(separator: "|")
+        return SortOrder(first: Int(nums[0])!, last: Int(nums[1])!)
+      })
+
+    orderRules = [:]
+
+    for s in sortOrders {
+      var orderRule = orderRules[s.first, default: OrderRule(before: [], after: [])]
+      orderRule.after.insert(s.last)
+      orderRules[s.first] = orderRule
+
+      orderRule = orderRules[s.last, default: OrderRule(before: [], after: [])]
+      orderRule.before.insert(s.first)
+      orderRules[s.last] = orderRule
+    }
+
+    updates = parts[1].split(separator: "\n")
+      .map({ line in
+        line.split(separator: ",").map { Int($0)! }
+      })
+  }
+
+  let data: String
+
+  let sortOrders: [SortOrder]
+  var orderRules: [Int: OrderRule]
+
+  let updates: [[Int]]
+
+  func isUpdateCorrectlyOrdered(update: [Int]) -> Bool {
+    for (i, num) in update.enumerated() {
+      guard let orderRule = orderRules[num] else {
+        continue
+      }
+
+      for ii in i..<update.count {
+        if orderRule.before.contains(update[ii]) {
+          return false
+        }
+      }
+    }
+
+    return true
+  }
+
+  func part1() -> Int {
+    return
+      updates.filter { isUpdateCorrectlyOrdered(update: $0) }
+      .map { $0[$0.count / 2] }
+      .reduce(0, +)
+  }
+
+  func part2() -> Int { return 0 }
+}
---
+++ 2024/Tests/Day05.swift
@@ -0,0 +1,59 @@
+import Testing
+
+@testable import AdventOfCode
+
+@Suite("Day 5") struct Day05Tests {
+
+  let input = """
+    47|53
+    97|13
+    97|61
+    97|47
+    75|29
+    61|13
+    75|53
+    29|13
+    97|29
+    53|29
+    61|53
+    97|53
+    61|29
+    47|13
+    75|47
+    97|75
+    47|61
+    75|61
+    47|29
+    75|13
+    53|13
+
+    75,47,61,53,29
+    97,61,53,29,13
+    75,29,13
+    75,97,47,61,53
+    61,13,29
+    97,13,75,29,47
+    """
+
+  @Test func ParsesInput() async throws {
+    let day5 = Day05(data: input)
+    #expect(
+      day5.updates == [
+        [75, 47, 61, 53, 29],
+        [97, 61, 53, 29, 13],
+        [75, 29, 13],
+        [75, 97, 47, 61, 53],
+        [61, 13, 29],
+        [97, 13, 75, 29, 47],
+      ]
+    )
+  }
+
+  @Test func Part1() async throws {
+    #expect(Day05(data: input).part1() == 143)
+  }
+
+  @Test func Part2() async throws {
+    //    #expect(Day05(data: input).part2() == 31)
+  }
+}
