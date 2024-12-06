fe91a6212c9f982a6fa958144c099936fa8a6830
Bryan Oltman <bryan@shorebird.dev>
2024-12-06 09:34:27 -0500

    2024 day 6 setup

---
+++ 2024/Sources/Day06.swift
@@ -0,0 +1,12 @@
+struct Day06: AdventDay {
+  var data: String
+
+
+  func part1() -> Int {
+    return 0
+  }
+
+  func part2() -> Int {
+    return 0
+  }
+}
---
+++ 2024/Tests/Day06.swift
@@ -0,0 +1,18 @@
+import Testing
+
+@testable import AdventOfCode
+
+@Suite("Day 6") struct Day06Tests {
+
+  let input = """
+    """
+
+  @Test func ParsesInput() async throws {
+  }
+
+  @Test func Part1() async throws {
+  }
+
+  @Test func Part2() async throws {
+  }
+}
