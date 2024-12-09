dc847ed5053d11bf53ef9459a8da096e96d89a59
Bryan Oltman <bryan@shorebird.dev>
2024-12-08 21:11:16 -0500

    2024 day 8 setup

---
+++ 2024/Tests/Day08.swift
@@ -0,0 +1,21 @@
+import Testing
+
+@testable import AdventOfCode
+
+@Suite("Day 8") struct Day08Tests {
+
+  let input = """
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
+    """
+
+  @Test func Part1() async throws {
+  }
+
+  @Test func Part2() async throws {
+  }
+}
