f7620bb9be8708e272f89e6e45af4bc145142a85
Bryan Oltman <bryan@shorebird.dev>
2024-12-11 11:49:27 -0500

    2024 day 11

--- 2024/Sources/Day11.swift
+++ 2024/Sources/Day11.swift
@@ -1,11 +1,82 @@
+struct StonesState {
+  let numZeros: Int
+  let numOnes: Int
+  // Mapping of stone numbers to how many of them we have
+  let otherStoneCounts: [Int: Int]
+
+  var totalStones: Int {
+    numZeros + numOnes + otherStoneCounts.values.reduce(0, +)
+  }
+}
+
 struct Day11: AdventDay {
   var data: String
 
+  var stones: [Int] {
+    data.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: " ").map { Int($0)! }
+  }
+
+  func hasEvenNumberOfDigits(_ number: Int) -> Bool {
+    var count = 0
+    var num = abs(number)
+
+    while num > 0 {
+      count += 1
+      num /= 10
+    }
+
+    return count % 2 == 0
+  }
+
+  func blink(stoneCounts originalCounts: [Int: Int]) -> [Int: Int] {
+    var newCounts = [Int: Int]()
+    for (stone, count) in originalCounts {
+      let stoneStr = String(stone)
+      if stone == 0 {
+        // Zeroes become ones
+        newCounts[1, default: 0] += count
+      }
+      else if stoneStr.count % 2 == 0 {
+        // Stones with an even number of digits are replaced by two stones
+        // 1234 -> 12 34
+        let middleIndex = stoneStr.index(stoneStr.startIndex, offsetBy: stoneStr.count / 2)
+        let firstHalf = String(stoneStr[stoneStr.startIndex..<middleIndex])
+        let secondHalf = String(stoneStr[middleIndex..<stoneStr.endIndex])
+        newCounts[Int(firstHalf)!, default: 0] += count
+        newCounts[Int(secondHalf)!, default: 0] += count
+      }
+      else {
+        // the old stone's number multiplied by 2024 is engraved on the new stone
+        newCounts[stone * 2024, default: 0] += count
+      }
+    }
+
+    return newCounts
+  }
+
   func part1() -> Int {
-    return 0
+    var stoneCounts = [Int: Int]()
+    for stone in stones {
+      stoneCounts[stone, default: 0] += 1
+    }
+
+    for _ in 0..<25 {
+      stoneCounts = blink(stoneCounts: stoneCounts)
+    }
+
+    return stoneCounts.values.reduce(0, +)
   }
 
   func part2() -> Int {
-    return 0
+    var stoneCounts = [Int: Int]()
+    for stone in stones {
+      stoneCounts[stone, default: 0] += 1
+    }
+
+    for _ in 0..<75 {
+      stoneCounts = blink(stoneCounts: stoneCounts)
+    }
+
+    return stoneCounts.values.reduce(0, +)
   }
 }
--- 2024/Tests/Day11.swift
+++ 2024/Tests/Day11.swift
@@ -3,25 +3,15 @@ import Testing
 @testable import AdventOfCode
 
 @Suite("Day 11") struct Day11Tests {
-  let input = """
-    89010123
-    78121874
-    87430965
-    96549874
-    45678903
-    32019012
-    01329801
-    10456732
-    """
-
+  let input = "125 17"
 
   @Test func Part1() {
     let day = Day11(data: input)
-    #expect(day.part1() == 0)
+    #expect(day.part1() == 55312)
   }
 
   @Test func Part2() {
     let day = Day11(data: input)
-    #expect(day.part2() == 0)
+    #expect(day.part2() == 65601038650482)
   }
 }
