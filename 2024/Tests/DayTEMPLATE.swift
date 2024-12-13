a1616fd5f00c12ef3cd105d3bb00a342f9494b1e
Bryan Oltman <bryan@shorebird.dev>
2024-12-13 11:41:49 -0500

    2024 add test template

---
+++ 2024/Tests/DayTEMPLATE.swift
@@ -0,0 +1,17 @@
+import Testing
+
+@testable import AdventOfCode
+
+@Suite("Day TODO") struct DayTODOTests {
+
+  let input = """
+    """
+
+  @Test func Part1() async throws {
+    #expect(DayTODO(data: input).part1() == 0)
+  }
+
+  @Test func Part2() async throws {
+    #expect(DayTODO(data: input).part2() == 0)
+  }
+}
