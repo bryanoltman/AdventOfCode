ad68b5931ac3316def0e6cb8b77943c76e85fb02
Bryan Oltman <bryan@shorebird.dev>
2024-12-13 23:00:47 -0500

    2024 day 12 part 01

--- 2024/Sources/Day12.swift
+++ 2024/Sources/Day12.swift
@@ -10,7 +10,8 @@ struct Day12: AdventDay {
       string: data,
       itemConverter: {
         $0
-      })
+      }
+    )
   }
 
   var data: String
@@ -19,19 +20,29 @@ struct Day12: AdventDay {
 
   func getRegions(grid: Grid<String>) -> [Region] {
     var unvisitedPoints = Set(grid.points)
+    var seenPoints = Set<Point>()
     var regions: [Region] = []
     while !unvisitedPoints.isEmpty {
       let startingPoint = unvisitedPoints.removeFirst()
+      if seenPoints.contains(startingPoint) {
+        continue
+      }
+
       let newRegionId = grid.at(point: startingPoint)
+
       var toVisit = [startingPoint]
       var regionPoints = Set<Point>()
       while !toVisit.isEmpty {
         let currentPoint = toVisit.removeFirst()
+        if seenPoints.contains(currentPoint) {
+          continue
+        }
+        seenPoints.insert(currentPoint)
         unvisitedPoints.remove(currentPoint)
         regionPoints.insert(currentPoint)
         let neighborsInPlot = grid.neighbors(point: currentPoint)
           .filter {
-            grid.at(point: $0) == newRegionId && unvisitedPoints.contains($0)
+            grid.at(point: $0) == newRegionId && !seenPoints.contains($0)
           }
         toVisit.append(contentsOf: neighborsInPlot)
       }
@@ -42,12 +53,41 @@ struct Day12: AdventDay {
     return regions
   }
 
+  func perimeter(points: Set<Point>) -> Int {
+    var perimeter = 0
+    for point in points {
+      let neighbors = [
+        Point(x: point.x - 1, y: point.y),
+        Point(x: point.x + 1, y: point.y),
+        Point(x: point.x, y: point.y - 1),
+        Point(x: point.x, y: point.y + 1),
+      ]
+
+      let numPresentNeighbors = neighbors.filter { points.contains($0) }.count
+      // Sides without neighbors add to perimeter
+      perimeter += 4 - numPresentNeighbors
+    }
+
+    return perimeter
+  }
+
+  func area(points: Set<Point>) -> Int {
+    points.count
+  }
+
   func part1() -> Int {
     let regions = getRegions(grid: grid)
+    return regions.map { perimeter(points: $0.points) * area(points: $0.points) }
+      .reduce(0, +)
+  }
+
+  func numSides(points: Set<Point>) -> Int {
     return 0
   }
 
   func part2() -> Int {
-    return 0
+    let regions = getRegions(grid: grid)
+    return regions.map { numSides(points: $0.points) * area(points: $0.points) }
+      .reduce(0, +)
   }
 }
