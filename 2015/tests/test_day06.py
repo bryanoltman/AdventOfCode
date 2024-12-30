126d8fa41aa7dc4ea0af40270ea999324764f6c5
bryanoltman <bryan@shorebird.dev>
2024-12-30 00:29:42 -0500

    2015 day 6 wip

---
+++ 2015/tests/test_day06.py
@@ -0,0 +1,14 @@
+from src.day06 import part1, part2
+
+
+def test_part1():
+    input = """
+turn on 0,0 through 999,999
+toggle 0,0 through 999,0
+turn off 499,499 through 500,500
+"""
+    assert part1(input) == 1_000_000 - 1000 - 4
+
+
+def test_part2():
+    pass
