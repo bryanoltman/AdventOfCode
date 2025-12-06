6d4f819c0c9caf4f48e63acddd73474101b37905
Bryan Oltman <bryan@shorebird.dev>
2025-12-06 13:08:12 -0500

    2025 06 ugly part 2 solution

--- 2025/06.test.ts
+++ 2025/06.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test";
-import { parseInput, part1, part2 } from "./06";
+import { parseInputPart1, parseInputPart2, part1, part2 } from "./06";
 
 const rawInput = `
 123 328  51 64 
@@ -7,12 +7,13 @@ const rawInput = `
   6 98  215 314
 *   +   *   +  `;
 
-let input = parseInput(rawInput);
+let inputPart1 = parseInputPart1(rawInput);
+let inputPart2 = parseInputPart2(rawInput);
 
 test("part1", () => {
-  expect(part1(input)).toBe(4277556);
+  expect(part1(inputPart1)).toBe(4277556);
 });
 
 test("part2", () => {
-  expect(part2(input)).toBe(0);
+  expect(part2(inputPart2)).toBe(3263827);
 });
--- 2025/06.ts
+++ 2025/06.ts
@@ -5,9 +5,12 @@ type WorksheetColumn = {
   operation: Operation;
 };
 
-type Input = WorksheetColumn[];
+type RawWorksheetColumn = {
+  numbers: string[];
+  operation: Operation;
+};
 
-export function parseInput(input: string): Input {
+export function parseInputPart1(input: string): WorksheetColumn[] {
   const lines = input
     .split("\n")
     .map((l) => l.trim())
@@ -35,20 +38,69 @@ function processColumn(column: WorksheetColumn): number {
   return column.numbers.reduce((acc, curr) => op(acc, curr));
 }
 
-export function part1(input: Input): number {
+export function part1(input: WorksheetColumn[]): number {
   return input
     .map((c) => processColumn(c))
     .reduce((acc, curr) => addOp(acc, curr));
 }
 
-export function part2(input: Input): number {
-  return 0;
+export function parseInputPart2(input: string): RawWorksheetColumn[] {
+  const lines = input.split("\n").filter((l) => l.length > 0);
+  const numLines = lines.slice(0, lines.length - 1);
+  const opLine = lines[lines.length - 1]!;
+
+  let ret = <RawWorksheetColumn[]>[];
+  let currIdx = 0;
+  while (currIdx < opLine.length) {
+    let op = opLine[currIdx] as Operation;
+    let nextIdx = currIdx + 1;
+    while (opLine[nextIdx] == " ") {
+      nextIdx += 1;
+    }
+    let columnWidth = nextIdx - currIdx;
+    // Subtract one to account for the single space padding between columns
+    if (nextIdx != opLine.length) {
+      columnWidth -= 1;
+    }
+
+    let numberStrings = <string[]>[];
+    for (const numLine of numLines) {
+      numberStrings.push(numLine.slice(currIdx, currIdx + columnWidth));
+    }
+    ret.push({
+      operation: op,
+      numbers: numberStrings,
+    });
+    currIdx = nextIdx;
+  }
+
+  return ret;
+}
+
+function processPart2Column(column: RawWorksheetColumn): number {
+  let numbers = <number[]>[];
+
+  for (let i = column.numbers[0]!.length - 1; i >= 0; i--) {
+    let currNumber = "";
+    for (const num of column.numbers) {
+      currNumber += num[i]!;
+    }
+    numbers.push(Number(currNumber));
+  }
+
+  const op = column.operation == "*" ? multOp : addOp;
+  return numbers.reduce((acc, curr) => op(acc, curr));
+}
+
+export function part2(input: RawWorksheetColumn[]): number {
+  return input.map(processPart2Column).reduce((acc, curr) => addOp(acc, curr));
 }
 
 const inputFile = Bun.file("./data/06.txt");
 const inputFileExists = await inputFile.exists();
 if (inputFileExists) {
-  const input = parseInput(await inputFile.text());
-  console.log(`part 1: ${part1(input)}`);
-  console.log(`part 2: ${part2(input)}`);
+  const inputPart1 = parseInputPart1(await inputFile.text());
+  console.log(`part 1: ${part1(inputPart1)}`);
+  const inputPart2 = parseInputPart2(await inputFile.text());
+  console.log(`part 2: ${part2(inputPart2)}`);
 }
